43427d72d148d9b40cb19efaa4e953903fb023f6
TuckOfArthas <use_curseforge_to_contact_me@email.com>
2011-01-20 15:46:10 +0100

    Cleared the default config

--- TuckBindingsCommon.lua
+++ TuckBindingsCommon.lua
@@ -1,15 +1,8 @@
+TuckBindings.common = {}
 local TB = TuckBindings
 
-TuckBindings.common = {}
 function TuckBindings.common:Init()
 
--- default actions: assist unit
-TB:Macro("CAPSLOCK", "/assist")
-TB:Macro("ALT-CAPSLOCK", "/assist focus")
-
--- default actions: focus targets, target swapping
-TB:SwapTargetFocus("§")
-TB:Macro("SHIFT-§","/focus")
-TB:Macro("ALT-§","/targetlasttarget")
+--TODO: Add all class-independent key bindings here
 
 end
