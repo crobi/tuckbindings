b3cc206eb0121e59dfd4bb4ac34fdc18605cf53f
TuckOfArthas <use_curseforge_to_contact_me@email.com>
2011-01-20 13:22:00 +0100

    More cleanup for stances

--- TuckBindings.lua
+++ TuckBindings.lua
@@ -10,7 +10,9 @@ local ERROR = function(msg) ChatFrame1:AddMessage(msg) end
   Global variables
 ]]
 TuckBindings = {btn_count = 0, buttons = {} }
+
 local TB = TuckBindings
+local human_form = ""
 
 --[[
   Converts the input into a table
@@ -29,98 +31,9 @@ end
 
 Macro(binding)
 Cast(binding, condition, targets, altcast)
-CastInStance(binding, condition, targets, altcast, stances)
-CastSwitchStance(binding, condition, targets, altcast, stances)
+CastNoShapeshift(binding, condition, targets, altcast, stances)
+CastShapeshift(binding, condition, targets, altcast, stances)
 
---[[
-
-]]
-function TuckBindings:CreateStealthButton(binding, stealthspell, spell, selfcast, focuscast)
-	TB:CreateMacroButton(binding, "/cast [stealth]"..stealthspell.."; "..spell)
-	
-	if (selfcast) then
-		TB:CreateMacroButton("SHIFT-"..binding, "/cast [@player, stealth] "..stealthspell.."; "..spell)
-	end
-	
-	if (focuscast) then
-		TB:CreateMacroButton("CTRL-"..binding, "/cast [@focus, stealth] "..stealthspell.."; [@focus] "..spell)
-	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateSpellButton(binding, spell, selfcast, focuscast)
-	TuckBindings:CreateMacroButton(binding, "/cast "..spell)
-	
-	if (selfcast) then
-		TuckBindings:CreateMacroButton("SHIFT-"..binding, "/cast [@player] "..spell)
-	end
-	
-	if (focuscast) then
-		TuckBindings:CreateMacroButton("CTRL-"..binding, "/cast [@focus] "..spell)
-	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateShapeshiftSpellButton(binding, spell, stance, shapename, selfcast, focuscast)
-	TuckBindings:CreateMacroButton(binding, "/cast [nostance: "..stance.."] "..shapename.."; "..spell)
-	
-	if (selfcast) then
-		TuckBindings:CreateMacroButton("SHIFT-"..binding, "/cast [nostance: "..stance.."] "..shapename.."; [@player] "..spell)
-	end
-	
-	if (focuscast) then
-		TuckBindings:CreateMacroButton("CTRL-"..binding, "/cast [nostance: "..stance.."] "..shapename.."; [@focus] "..spell)
-	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateNoShapeshiftSpellButton(binding, spell, selfcast, focuscast)
-	TuckBindings:CreateMacroButton(binding, "/cancelform [stance]\n/cast "..spell)
-	
-	if (selfcast) then
-		TuckBindings:CreateMacroButton("SHIFT-"..binding, "/cancelform [stance]\n/cast [@player]"..spell)
-	end
-	
-	if (focuscast) then
-		TuckBindings:CreateMacroButton("CTRL-"..binding, "/cancelform [stance]\n/cast [@player]"..spell)
-	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateNoShapeshiftHelpButton(binding, spell, target2, selfcast)
-	if (target2) then
-		TuckBindings:CreateMacroButton(binding, "/cancelform [stance]\n/cast [help]"..spell.."; [help, @"..target2.."] "..spell)
-	else
-		TuckBindings:CreateMacroButton(binding, "/cancelform [stance]\n/cast [help]"..spell)
-	end
-	
-	if (selfcast) then
-		TuckBindings:CreateMacroButton(selfcast.."-"..binding, "/cancelform [stance]\n/cast [@player]"..spell)
-	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateNoShapeshiftHarmButton(binding, spell, target2, selfcast)
-	if (target2) then
-		TuckBindings:CreateMacroButton(binding, "/cancelform [stance]\n/cast [harm]"..spell.."; [harm, @"..target2.."] "..spell)
-	else
-		TuckBindings:CreateMacroButton(binding, "/cancelform [stance]\n/cast [harm]"..spell)
-	end
-	
-	if (selfcast) then
-		TuckBindings:CreateMacroButton(selfcast.."-"..binding, "/cancelform [stance]\n/cast [harm, @focus]"..spell)
-	end
-end
 
 --[[
 
@@ -135,79 +48,6 @@ function TuckBindings:CreateItemButton(binding, item, selfcast, focuscast)
 	if (focuscast) then
 		TuckBindings:CreateMacroButton("CTRL-"..binding, "/use [@focus] "..item)
 	end
-end
-
---[[
-
-]]
-function TuckBindings:CreateStanceButton(binding, mod,
-    nostance_target, nostance_spell, mod_nostance_target, mod_nostance_spell,
-    stance1, stance1_target, stance1_spell, mod_stance1_target, mod_stance1_spell, 
-    stance2, stance2_target, stance2_spell, mod_stance2_target, mod_stance2_spell) 
-
-    local macro_string = "/cast "
-    
-    -- default: focuscast
-    if (stance1 and stance1_target and stance1_spell) then
-        macro_string = macro_string.."[stance:"..stance1..", help] "..stance1_spell.."; "
-        macro_string = macro_string.."[stance:"..stance1..", @"..stance1_target.."] "..stance1_spell.."; "
-    end
-    
-    if (stance2 and stance2_target and stance2_spell) then
-        macro_string = macro_string.."[stance:"..stance2..", help] "..stance2_spell.."; "
-        macro_string = macro_string.."[stance:"..stance2..", @"..stance2_target.."] "..stance2_spell.."; "
-    end
-    
-    if (nostance_target and nostance_spell) then
-        macro_string = macro_string.."[help] "..nostance_spell.."; "
-        macro_string = macro_string.."[@"..nostance_target.."] "..nostance_spell
-    end
-    
-    TuckBindings:CreateMacroButton(binding, macro_string)
-    
-    
-    -- Modifier Key
-    if (mod) then
-
-        macro_string = "/cast "
-
-        if (stance1 and mod_stance1_target and mod_stance1_spell) then
-            macro_string = macro_string.."[stance:"..stance1..", @"..mod_stance1_target.."] "..mod_stance1_spell.."; "
-        end
-        
-        if (stance2 and mod_stance2_target and mod_stance2_spell) then
-            macro_string = macro_string.."[stance:"..stance2..", @"..mod_stance2_target.."] "..mod_stance2_spell.."; "
-        end
-
-        if (mod_nostance_target and mod_nostance_spell) then
-            macro_string = macro_string.."[@"..mod_nostance_target.."] "..mod_nostance_spell
-        end
-        
-        TuckBindings:CreateMacroButton("SHIFT-"..binding, macro_string)
-
-    end
-
-end
-
---[[
-    Returns the index of the given stance
-]]
-function TuckBindings:StanceToIndex(stance_name)
-    --ChatFrame1:AddMessage("Getting stance index of: "..stance_name)
-    if stance_name == nil then
-        return 0
-    end
-    
-    local i
-    local stance_count = GetNumShapeshiftForms()
-    for i = 1, stance_count do
-        local icon, name = GetShapeshiftFormInfo(i);
-        if name == stance_name then
-            return i
-        end
-    end
-    
-    return nil
 end
 
 --[[
@@ -228,7 +68,7 @@ function TuckBindings:ConcatenateConditions(condition)
 end
 
 --[[
-    Returns a string of the form "[target=target, condition_string]" or "[target=target1, condition_string][target=target2, condition_string]..."
+    Returns a string of the form "[@target, condition_string]" or "[@target1, condition_string][@target2, condition_string]..."
     Input: nil or string or table of strings
 ]]
 function TuckBindings:ConcatenateTargets(target, condition_string)
@@ -245,40 +85,72 @@ function TuckBindings:ConcatenateTargets(target, condition_string)
 end
 
 --[[
+    Returns true if the given stance name corresponds to the human form/no stance
+]]
+function TuckBindings::IsHumanForm(stance_name)
+	return stance_name == human_form or stance_name == "Human"
+end
+
+--[[
+    Returns the index of the given stance
+]]
+function TuckBindings:StanceToIndex(stance_name)
+	if stance_name == nil  then
+		return nil
+	end
+	
+	if TB:IsHumanForm(stance_name) then
+		return 0
+	end
+
+	local i
+	local stance_count = GetNumShapeshiftForms()
+	for i = 1, stance_count do
+		local icon, name = GetShapeshiftFormInfo(i);
+		if name == stance_name then
+			return i
+		end
+	end
+
+	return nil
+end
+
+--[[
     Returns a string of the form "" or "stance:1" or "stance:1/2/3..."
     Input: nil or string or table of strings
 ]]
 function TuckBindings:CreateStanceString(stance)
 	-- stance handling stuff
 	if stance == nil then
-		return "", ""
+		return nil, nil
 	else
 		local stance_list = ""
-		local first_stance = ""
+		local first_stance = nil
 		local stance_count = 0
 		if type(stance) == "table" then
 			for i, v in ipairs(stance) do
-				local stance_index = TuckBindings:StanceToIndex(v)
-				if stance_index then
+				local stance_index = TB:StanceToIndex(v)
+				if stance_index ~= nil then
 					if stance_count > 0 then
 						stance_list = stance_list.. "/"
 					end
 					stance_list = stance_list..stance_index
 					stance_count = stance_count + 1
-					if first_stance == "" then
+					if first_stance == nil then
 						first_stance = v
 					end
 				end
 			end
 		else
-			stance_list = TuckBindings:StanceToIndex(stance)
+			stance_list = TB:StanceToIndex(stance)
 			first_stance = stance
+			stance_count = 1
 		end
 
-		if stance_list then
+		if stance_count > 0 then
 			return "stance:"..stance_list, first_stance
 		else
-			return "", ""
+			return nil, nil
 		end
 	end
 end
@@ -290,7 +162,9 @@ end
 ]]
 function TuckBindings:CreateStanceSwitchCastString(stance)
 	local stance_string, first_stance = CreateStanceString(stance)
-	if stance_string ~= "" then
+	if stance_string and TB:IsHumanForm(first_stance) then
+		return "/cancelform [no"..stance_string.."]\n/cast "
+	elseif stance_string then
 		return "/cast [no"..stance_string.."]"..first_stance.."; "
 	else
 		return "/cast "
