715e26001bc0928238f7b3b0b33e08f5707a105b
TuckOfArthas <use_curseforge_to_contact_me@email.com>
2011-02-07 23:11:20 +0100

    Remove old key bindings for the default configuration

--- TuckBindingsMage.lua
+++ TuckBindingsMage.lua
@@ -2,33 +2,6 @@ TuckBindings.MAGE = {}
 
 function TuckBindings.MAGE:Init()
 
--- attack
-TuckBindings:CreateMacroButton("T",		   "/startattack")
-TuckBindings:CreateMacroButton("SHIFT-T",  "/startattack focus")
-TuckBindings:CreateMacroButton("1",		   "/cast Shoot")
-TuckBindings:CreateMacroButton("CAPSLOCK", "/assist")
-TuckBindings:CreateMacroButton("SHIFT-CAPSLOCK", "/assist focus")
+--TODO: Add your key binding configuration here
 
--- damage
-TuckBindings:CreateSpellButton("2",        "Frostbolt")
-TuckBindings:CreateSpellButton("E",        "Fireball")
-TuckBindings:CreateSpellButton("SHIFT-E",  "Pyroblast")
-TuckBindings:CreateSpellButton("4",		   "Scorch")
-TuckBindings:CreateSpellButton("R",        "Fire Blast")
-TuckBindings:CreateSpellButton("SHIFT-R",  "Cone of Cold")
-TuckBindings:CreateSpellButton("SHIFT-C",  "Frost Nova")
-TuckBindings:CreateSpellButton("F",        "Arcane Explosion")
-TuckBindings:CreateSpellButton("7",        "Rain of Fire")
-
--- misc
-TuckBindings:CreateSpellButton("C",        "Blink")
-TuckBindings:CreateMacroButton("Q",        "/cast [nodead, harm, target=focus] Polymorph; Polymorph")
-
-
--- buffs
-TuckBindings:CreateSpellButton("0",		   "Mana Shield")
-TuckBindings:CreateSpellButton("-",		   "Dampen Magic", true, true)
-TuckBindings:CreateSpellButton("SHIFT--",  "Amplify Magic", true, true)
-TuckBindings:CreateSpellButton("=",		   "Arcane Intellect", true, true)
-TuckBindings:CreateSpellButton("SHIFT-=",  "Arcane Brilliance", true, true)
 end
